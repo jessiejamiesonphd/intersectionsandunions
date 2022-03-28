7292957692b1183bf0a140de9f64451266239984
Jessie Jamieson <jessie.d.jamieson@gmail.com>
2022-03-28 14:07:47 -0400

    Updated the config file to use extensions for my blog

--- source/conf.py
+++ source/conf.py
@@ -17,8 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'intersections-and-unions'
-copyright = '2022, jessiejamiesonphd'
+project = 'Intersections and Unions'
+copyright = '2022, Jessie D. Jamieson, PhD'
 author = 'jessiejamiesonphd'
 
 # The full version, including alpha/beta/rc tags
@@ -31,6 +31,10 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+"ablog",
+"sphinx.ext.intersphinx",
+"sphinx.ext.mathjax",
+"sphinx_panels",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- source/index.rst
+++ source/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to intersections-and-unions's documentation!
+Welcome to Intersections and Unions, a blog exploring how math plays well with others!
 ====================================================
 
 .. toctree::
